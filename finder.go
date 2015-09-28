45c0c9d864154ed57878300ba54d7cf170b1c7a2
Jason Stillwell <jstillwell@zendesk.com>
2015-09-28 00:21:09 -0700

    use the steamid as the key to the datastore. make one call to the steam api, instead of one for each user. a little refactoring for better names. fix the bug: creating a new record each save.

--- finder.go
+++ finder.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"appengine"
@@ -41,7 +42,7 @@ $ curl 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=XXX
 	}
 */
 
-func getEntityKey(c appengine.Context) *datastore.Key {
+func getEntityRootKey(c appengine.Context) *datastore.Key {
 	return datastore.NewKey(c, "Summaries", "default_summaries", 0, nil)
 }
 
@@ -51,80 +52,100 @@ func init() {
 	http.HandleFunc("/", handler)
 }
 
-type Summary struct {
+type PlayerSummariesResult struct {
+	Response struct {
+		Players []struct {
+			Steamid                  int64 `json:"string"`
+			Communityvisibilitystate uint
+			Profilestate             uint
+			Personaname              string
+			Lastlogoff               uint
+			Profileurl               string
+			Avatar                   string
+			Avatarmedium             string
+			Avatarfull               string
+			Personastate             uint
+			Realname                 string
+			Primaryclanid            uint64 `json:"string"`
+			Timecreated              uint
+			Personastateflags        uint
+			Gameextrainfo            string
+			Gameid                   uint `json:"string"`
+			Loccountrycode           string
+		}
+	}
+}
+
+type StoredSummary struct {
 	Steamid int64
 	Online  bool
+}
+
+func getPlayerSteamIdsString() string {
+	ss := make([]string, len(steamids))
+	for i, s := range steamids {
+		ss[i] = fmt.Sprintf("%d", s)
+	}
+	return strings.Join(ss, ",")
+}
+
+func getPlayerCount() int {
+	return len(steamids)
+}
+
+func fetchPlayerSummaries(c appengine.Context) (*PlayerSummariesResult, error) {
+
+	client := urlfetch.Client(c)
+
+	url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s", TOKEN, getPlayerSteamIdsString())
+	c.Debugf("steam API url: %v", url)
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failure of steam API: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code was not 200 (%d)", resp.StatusCode)
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	response := PlayerSummariesResult{}
+
+	err = json.Unmarshal(buf, &response)
+	if err != nil {
+		return nil, fmt.Errorf("failure to read json %v", err)
+	}
+
+	if len(response.Response.Players) == 0 {
+		return nil, fmt.Errorf("not enough players in the response %d != %d", len(response.Response.Players), getPlayerCount())
+	}
+
+	if len(response.Response.Players) > getPlayerCount() {
+		return nil, fmt.Errorf("too many players in the response %d != %d", len(response.Response.Players), getPlayerCount())
+	}
+
+	return &response, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	c := appengine.NewContext(r)
 
-	client := urlfetch.Client(c)
+	summaries, err := fetchPlayerSummaries(c)
+	if err != nil {
+		c.Errorf("failed to retrieve player status: %v", err)
+		return
+	}
 
-	for _, steamid := range steamids {
+	for _, playerSummary := range summaries.Response.Players {
 
-		url := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%d", TOKEN, steamid)
-		resp, err := client.Get(url)
-		if err != nil {
-			c.Errorf("failure of steam API: %v", err)
-			return
-		}
+		playerName := playerSummary.Personaname
 
-		if resp.StatusCode != 200 {
-			c.Errorf("status code was not 200 (%d)", resp.StatusCode)
-			return
-		}
-
-		buf, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-			return
-		}
-
-		response := struct {
-			Response struct {
-				Players []struct {
-					Steamid                  int64 `json:"string"`
-					Communityvisibilitystate uint
-					Profilestate             uint
-					Personaname              string
-					Lastlogoff               uint
-					Profileurl               string
-					Avatar                   string
-					Avatarmedium             string
-					Avatarfull               string
-					Personastate             uint
-					Realname                 string
-					Primaryclanid            uint64 `json:"string"`
-					Timecreated              uint
-					Personastateflags        uint
-					Gameextrainfo            string
-					Gameid                   uint `json:"string"`
-					Loccountrycode           string
-				}
-			}
-		}{}
-
-		err = json.Unmarshal(buf, &response)
-		if err != nil {
-			c.Errorf("failure to read json %v", err)
-			return
-		}
-
-		if len(response.Response.Players) == 0 {
-			c.Errorf("not enough players in the response")
-			return
-		}
-
-		if len(response.Response.Players) > 1 {
-			c.Errorf("too many players in the response")
-			return
-		}
-
-		playerName := response.Response.Players[0].Personaname
-
-		online := response.Response.Players[0].Gameextrainfo == "Team Fortress 2"
+		online := playerSummary.Gameextrainfo == "Team Fortress 2"
 
 		if online {
 			c.Debugf("%v %s is Online", playerName, time.Now())
@@ -132,7 +153,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			c.Debugf("%v %s is Offline", playerName, time.Now())
 		}
 
-		record, err := GetRecord(c, steamid)
+		record, err := GetRecord(c, playerSummary.Steamid)
 		if err != nil {
 			c.Errorf("failure to get record %v", err)
 			return
@@ -165,24 +186,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetRecord(c appengine.Context, steamid int64) (Summary, error) {
+func GetRecord(c appengine.Context, steamid int64) (StoredSummary, error) {
 
-	summaries := make([]Summary, 0, 1)
-	_, err := datastore.NewQuery(KIND).Ancestor(getEntityKey(c)).Filter("Steamid =", steamid).GetAll(c, &summaries)
+	summaries := make([]StoredSummary, 0, 1)
+	_, err := datastore.NewQuery(KIND).Ancestor(getEntityRootKey(c)).Filter("__key__ =", getEntityKey(c, steamid)).GetAll(c, &summaries)
 	if err != nil {
-		return Summary{}, err
+		return StoredSummary{}, err
 	}
 
 	if len(summaries) == 0 {
-		return Summary{Steamid: steamid, Online: false}, nil
+		return StoredSummary{Steamid: steamid, Online: false}, nil
 	} else {
 		return summaries[0], nil
 	}
 }
 
-func SaveRecord(c appengine.Context, record Summary) error {
+func getEntityKey(c appengine.Context, steamId int64) *datastore.Key {
+	return datastore.NewKey(c, KIND, "", int64(steamId), getEntityRootKey(c))
+}
 
-	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, KIND, getEntityKey(c)), &record)
-
+func SaveRecord(c appengine.Context, record StoredSummary) error {
+	_, err := datastore.Put(c, getEntityKey(c, record.Steamid), &record)
 	return err
 }
